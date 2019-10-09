b41c3495e73237016bf875bcdf74935c1339206b
Vlad <mobila1997@mail.ru>
2019-10-09 08:44:18 +0300

    figureclass

--- src/main/java/Figure.java
+++ src/main/java/Figure.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import javafx.scene.shape.Rectangle;
 
-public class Figure {
+public class Figure extends Rectangle {
+    private boolean markX = false;
+    private boolean markO = false;
+
+    public Figure() {
+    }
+
+    public Figure(boolean markX, boolean markO) {
+        this.markX = markX;
+        this.markO = markO;
+    }
+
+    public void take(boolean markX) {
+        this.markX = markX;
+        this.markO = !markX;
+    }
+
+    public boolean hasMarkX() {
+        return this.markX;
+    }
+
+    public boolean hasMarkO() {
+        return this.markO;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Figure{" +
+                "markX=" + markX +
+                ", markO=" + markO +
+                '}';
+    }
 }
